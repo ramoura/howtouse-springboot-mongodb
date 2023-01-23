5a56fa7dcfaeb3ffd042b824c6d08f3c34d4df7c
Renato Alessandro de Moura <renatomoura@gmail.com>
2023-01-23 20:44:55 -0300

    chore: Rename package and application class.

--- src/test/java/io/ramoura/howtouse/springboot/mongodb/ApplicationTests.java
+++ src/test/java/io/ramoura/howtouse/springboot/mongodb/ApplicationTests.java
@@ -1,10 +1,10 @@
-package io.ramoura.howtouse.springboot.mongodb.howtousespringbootmongodb;
+package io.ramoura.howtouse.springboot.mongodb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HowtouseSpringbootMongodbApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
