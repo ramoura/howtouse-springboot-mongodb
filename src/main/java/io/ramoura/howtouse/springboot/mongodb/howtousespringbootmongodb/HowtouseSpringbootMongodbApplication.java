6d44181a95c8a809824be3f3408b001cddf2d8ac
Renato Alessandro de Moura <renatomoura@gmail.com>
2023-01-23 20:37:23 -0300

    first commit

---
+++ src/main/java/io/ramoura/howtouse/springboot/mongodb/howtousespringbootmongodb/HowtouseSpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package io.ramoura.howtouse.springboot.mongodb.howtousespringbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowtouseSpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowtouseSpringbootMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ramoura/howtouse/springboot/mongodb/howtousespringbootmongodb/HowtouseSpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ramoura.howtouse.springboot.mongodb.howtousespringbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowtouseSpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
